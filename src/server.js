78bfa964499b0a34c60ee64272680c3ce7c11584
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-02-17 12:00:00 +0700

    Tambahkan penangan ./routes tambahan dan koneksi db di server.js

--- src/server.js
+++ src/server.js
@@ -2,3 +2,17 @@ const express = require('express');
 const sequelize = require('./konfigurasi/db');
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use('/pengguna', rutePengguna);
+app.use('/transaksi', ruteTransaksi);
+app.use('/akun', ruteAkun);
+
+sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`Server berjalan di http://localhost:${port}`);
+    });
+}).catch(error => {
+    console.error('Tidak bisa menyambungkan ke database:', error);
+});
