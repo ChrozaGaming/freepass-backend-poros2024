33662be04af7a42dd54f163a6730af82c43cb12f
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-02-17 12:15:58 +0700

    Add User model
    Model User.js ditambahkan untuk sequalize di file src/models. Ini mencakup params untuk id, name, email, dan password dengan tipe data beserta batasannya juga pada masing-masing params. Ini akan mengatur struktur untuk semua entries data Pengguna dalam database.

--- src/models/User.js
+++ src/models/User.js
@@ -1,2 +1,26 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
