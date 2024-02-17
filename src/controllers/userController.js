988d804d5752d07802c3bf94f6c69067dc144020
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-02-17 14:14:38 +0700

    Tambahkan fungsi getUserById ke userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,3 +8,16 @@ exports.getAllUsers = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
